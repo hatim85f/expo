283870186bcc38b2fd4875244c96f8f34b8577b0
evanbacon <bacon@expo.io>
2023-08-22 10:44:05 -0400

    fix assets transform in runtime

--- packages/expo-asset/tools/hashAssetFiles.js
+++ packages/expo-asset/tools/hashAssetFiles.js
@@ -3,8 +3,11 @@
 const md5File = require('md5-file/promise');
 
 module.exports = function hashAssetFiles(asset) {
-  return Promise.all(asset.files.map(md5File)).then(hashes => {
+  return Promise.all(asset.files.map(md5File)).then((hashes) => {
     asset.fileHashes = hashes;
+    if (process.env.NODE_ENV === 'production') {
+      asset.httpServerLocation = asset.httpServerLocation.replace(/\.\.\//g, '_');
+    }
     return asset;
   });
 };
