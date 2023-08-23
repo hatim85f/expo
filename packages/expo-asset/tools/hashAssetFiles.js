d542ef3b8e2fdd73b7b790744608538d1f57de33
evanbacon <bacon@expo.io>
2023-08-23 18:50:17 -0500

    working

--- packages/expo-asset/tools/hashAssetFiles.js
+++ packages/expo-asset/tools/hashAssetFiles.js
@@ -9,7 +9,12 @@ module.exports = function hashAssetFiles(asset) {
     // Convert the `../` segments of the server URL to `_` to support monorepos.
     // This same transformation takes place in `AssetSourceResolver.web` (expo-assets, expo-image) and `persistMetroAssets` of Expo CLI,
     // this originally came from the Metro opinion https://github.com/react-native-community/cli/blob/2204d357379e2067cebe2791e90388f7e97fc5f5/packages/cli-plugin-metro/src/commands/bundle/getAssetDestPathIOS.ts#L19C5-L19C10
-    asset.httpServerLocation = asset.httpServerLocation.replace(/\.\.\//g, '_');
+    if (asset.httpServerLocation.includes('?export_path=')) {
+      asset.httpServerLocation = asset.httpServerLocation
+        .match(/\?export_path=(.*)/)[1]
+        .replace(/\.\.\//g, '_');
+    }
+    // asset.httpServerLocation = asset.httpServerLocation.replace(/\.\.\//g, '_');
 
     return asset;
   });
